09f7d3909aa7bd059ecad2b9c26fc990af2bdbb8
Jonathan Soares <jonathan.slp32@yahoo.com>
2020-03-04 21:21:17 -0300

    Acrescentada mensagem de boa noite

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace teste
         static void Main(string[] args)
         {
             Console.WriteLine("Olá mundo!");
+            Console.WriteLine("Boa noite!");
 
             Console.ReadLine();
         }
